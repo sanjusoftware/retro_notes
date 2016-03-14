08dc20bee65e951632796f7088938209caa16d04
Sanjeev Mishra <sanjusoftware@gmail.com>
2016-03-14 19:46:54 +0530

    added landing page theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || (root_path)
+    stored_location_for(resource) || (user_retro_boards_path(current_user))
   end
 
   def handle_401_ajax
