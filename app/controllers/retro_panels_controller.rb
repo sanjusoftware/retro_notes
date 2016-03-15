c03f1553068d6b3f6c803cb90022ff13ef148951
Sanjeev Mishra <sanjusoftware@gmail.com>
2016-03-15 21:47:59 +0530

    added way to add new retro panel

--- app/controllers/retro_panels_controller.rb
+++ app/controllers/retro_panels_controller.rb
@@ -1,11 +1,27 @@
 class RetroPanelsController < ApplicationController
 
-  before_action :set_retro_panel
+  before_action :set_retro_panel, only: [:update, :destroy]
 
   def update
     respond_to do |format|
       if @retro_panel.update(retro_panel_params)
         format.json { head :no_content }
+      else
+        format.json { render json: @retro_panel.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+
+    @retro_board = RetroBoard.find(params[:retro_board_id])
+    @retro_panel = RetroPanel.new(retro_panel_params)
+    @retro_panel.retro_board = @retro_board
+
+    binding.pry
+    respond_to do |format|
+      if @retro_panel.save
+        format.js
       else
         format.json { render json: @retro_panel.errors, status: :unprocessable_entity }
       end
